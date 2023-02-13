01c2fe304663defe1b73410ed650775ca7d549a6
Innocent fiston Kabalisa <kabalisafiston@Innocents-MacBook-Pro.local>
2023-02-13 21:39:46 +0200

    initial commit

---
+++ src/navigation/LinkingConfiguration.ts
@@ -0,0 +1,24 @@
+/**
+ * Learn more about deep linking with React Navigation
+ * https://reactnavigation.org/docs/deep-linking
+ * https://reactnavigation.org/docs/configuring-links
+ */
+
+import { LinkingOptions } from '@react-navigation/native';
+import * as Linking from 'expo-linking';
+
+import { RootStackParamList } from '../../types';
+
+const linking: LinkingOptions<RootStackParamList> = {
+  prefixes: [Linking.createURL('/')],
+  config: {
+    screens: {
+      Login: 'login',
+      Singup: 'singup',
+      Modal: 'modal',
+      NotFound: '*',
+    },
+  },
+};
+
+export default linking;
---
+++ src/screens/TabOneScreen.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootStackScreenProps } from "../../types";
+
+export default function TabOneScreen({
+  navigation,
+}: RootStackScreenProps<"Login">) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <TouchableOpacity onPress={() => {
+        navigation.navigate('Singup');
+      }}>
+        <Text>signup</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+});
